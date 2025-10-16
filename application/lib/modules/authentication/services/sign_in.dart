2f52020b10c2cfb331efc7e888d3f78b52d64512
Zaporozhets Yura <works.yura@gmail.com>
2025-10-17 02:33:38 +0300

    Add shared_analytics

--- application/lib/modules/authentication/services/sign_in.dart
+++ application/lib/modules/authentication/services/sign_in.dart
@@ -1,1 +1,9 @@
-abstract interface class SignInService {}
+// abstract interface class SignInService {}
+abstract interface class SignInService {
+  Future<void> send(String username, String password);
+}
+
+class SignInServiceImpl implements SignInService {
+  @override
+  Future<void> send(String username, String password) async {}
+}
