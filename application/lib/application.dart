cdd90f27a8e56e232e980b412066febf82dbe26b
Zaporozhets Yura <works.yura@gmail.com>
2025-10-17 02:40:21 +0300

    add src

--- application/lib/application.dart
+++ application/lib/application.dart
@@ -1,2 +1,2 @@
-export 'screens/onboarding/widgets/onboarding.dart';
-export 'screens/dashboard/widgets/dashboard.dart';
+export 'src/screens/onboarding/widgets/onboarding.dart';
+export 'src/screens/dashboard/widgets/dashboard.dart';
